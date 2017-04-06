201b1de98a5d8eac8d0b46fabb07dca34a021704
Ginue <ginue02@uw.edu>
2017-04-05 17:22:54 -0700

    Layed out function for exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,15 +2,24 @@
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(a,b) {
+  # function to take in inputs
+  # function to get length
+  #function to subtract them. take absoluavlue 
+  # use paste function for sentence
+  N <- b-a
+  return ("The difference in lengths is " + N)
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-
+CompareLength(3, 7)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
-
+DescribeDefference <- function(a, b) {
+  
+}
 
 # Pass two vectors to your `DescribeDifference` function
 
